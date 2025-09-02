3351553f688a0d31a8a7af7ce3716a332f9bbd35
Svetlana Kryukova <svetlana.kryukova.job@gmail.com>
2025-09-02 19:16:50 +0300

    feat: add homepage and article page components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ArticlePageComponent } from './components/article-page/article-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'article', component: ArticlePageComponent },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/components/article-page/article-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-page',
+  standalone: true,
+  templateUrl: './article-page.component.html',
+  styleUrl: './article-page.component.scss'
+})
+export class ArticlePageComponent {
+
+}
