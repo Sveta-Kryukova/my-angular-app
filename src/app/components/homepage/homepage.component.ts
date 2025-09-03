e58dced074c8d39fb50ea46280607b0caa7567c4
Svetlana Kryukova <svetlana.kryukova.job@gmail.com>
2025-09-03 10:30:48 +0300

    feat: added search component and added color variables

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
+  imports: [SearchComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss',
+})
+export class SearchComponent {
+  searchValue = '';
+}
